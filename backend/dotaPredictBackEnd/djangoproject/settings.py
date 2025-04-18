0f4aedba39ce813553e6b7946374dfd5a69851ae
pho-rg <phdorglandes@hotmail.fr>
2025-04-18 11:08:51 +0200

    database settings

--- backend/dotaPredictBackEnd/djangoproject/settings.py
+++ backend/dotaPredictBackEnd/djangoproject/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
+import os # interact with operating system
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -37,6 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'djangoapp',
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
@@ -74,8 +77,12 @@ WSGI_APPLICATION = 'djangoproject.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': os.environ.get('DB_DRIVER', 'django.db.backends.postgresql'),
+        'NAME': os.environ.get('DOTAPREDICT_DB', 'postgres'),
+        'USER': os.environ.get('DOTAPREDICT_USER', 'postgres'),
+        'PASSWORD': os.environ.get('DOTAPREDICT_PASSWORD', 'postgres'),
+        'HOST': os.environ.get('DOTAPREDICT_HOST', 'localhost'),
+        'PORT': os.environ.get('DOTAPREDICT_PORT', '5432'),
     }
 }
 
