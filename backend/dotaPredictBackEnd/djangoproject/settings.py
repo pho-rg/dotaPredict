9d45e4dd2600ff9558d274061772506f63633337
pho-rg <phdorglandes@hotmail.fr>
2025-06-03 18:53:02 +0200

    few settings

--- backend/dotaPredictBackEnd/djangoproject/settings.py
+++ backend/dotaPredictBackEnd/djangoproject/settings.py
@@ -52,7 +52,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'dotaPredictBackEnd.urls'
+ROOT_URLCONF = 'djangoapp.urls'
 
 TEMPLATES = [
     {
