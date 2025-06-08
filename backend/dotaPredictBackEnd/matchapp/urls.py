5a9ef36781507987301680ae932a185300cd876a
pho-rg <phdorglandes@hotmail.fr>
2025-06-08 16:21:31 +0200

    retrieve matches raw json from steam api, update python image, .env files created

--- backend/dotaPredictBackEnd/matchapp/urls.py
+++ backend/dotaPredictBackEnd/matchapp/urls.py
@@ -1,6 +1,12 @@
 # matchapp/urls.py
 from django.urls import path
 
+from . import views
+
 urlpatterns = [
-    # futures routes ici
+    path('steam/GetLiveLeagueGames', views.SteamGetLiveLeagueGames),
+    #path('getMatches/<str:pk>/', views.getMatches),
+    #path('getMatch/<str:pk>/', views.getMatch),
+    #path('getMatchesInDraft/', views.getInDraftMatches),
+    #path('isMatchInDraft/<str:pk>/', views.isMatchInDraft),
 ]
