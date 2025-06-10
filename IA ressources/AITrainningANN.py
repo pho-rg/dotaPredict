fec3bef568a67094a4b8811f4f9a04cc0e4acb4a
issam <issame.hassane@hotmail.com>
2025-06-10 23:55:20 +0200

    ajout d'un model d'IA ANN

---
+++ IA ressources/AITrainningANN.py
@@ -0,0 +1,78 @@
+import json
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score, classification_report
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense
+
+import xgboost as xgb
+
+def openJson(path):
+    with open (path) as json_file:
+        data = json.load(json_file)
+    return data
+
+def transformationHeroes(row, team_col, team):
+    dfOut = pd.Series([False] * 146, index=[f'picks_team_{team}_{i}' for i in range(146)])
+    for hero_id in row[team_col]:
+        dfOut[f'picks_team_{team}_{hero_id}'] = True
+    return dfOut
+
+def data_frame(datas):
+    return pd.DataFrame({
+        'picks_team_0': [datas[0]],
+        'picks_team_1': [datas[1]]
+    })
+
+def saveDataFrameToJson(matches):
+    """Methode pour suvgarder les details des matches pros"""
+
+    fileName = f"TestDfML.json"
+    path = fr"C:\Users\issam\Documents\ProjetAnnuelAI\DotaMatchsData\{fileName}"
+
+    matches.to_json(path, index=False)
+
+def main():
+
+    match1=[[38,103,27,94,55],[10,64,131,19,129]]
+    dfTestMatch = data_frame(match1)
+
+    df_testM_match_dire = dfTestMatch.apply(lambda row: transformationHeroes(row, 'picks_team_1', 'dire'), axis=1)
+    df_testM_match_radiant = dfTestMatch.apply(lambda row: transformationHeroes(row, 'picks_team_0', 'radiant'), axis=1)
+    dfTestMatch = pd.concat([df_testM_match_radiant, df_testM_match_dire], axis=1)
+
+
+    matches = openJson(r"C:\Users\issam\Documents\ProjetAnnuelAI\DotaMatchsData\SauvgardeProMatchs2025-05-21.json")
+    df = pd.DataFrame(matches)
+
+    df_transformedRadiant = df.apply(lambda row: transformationHeroes(row, 'picks_id_team_0', 'radiant'), axis=1)
+    df_transformedDire = df.apply(lambda row: transformationHeroes(row, 'picks_id_team_1', 'dire'), axis=1)
+    dfTest = pd.concat([df_transformedRadiant, df_transformedDire], axis=1)
+
+    print('debut Apprentissage')
+
+    X=dfTest
+    y=df['radiant_win']
+    X_train , X_test, y_train, y_test = train_test_split(X, y, test_size=0.2,random_state=42)
+
+    model = Sequential()
+    model.add(Dense(32,input_dim=X_train.shape[1],activation='relu'))
+    model.add(Dense(32,activation='relu'))
+    model.add(Dense(32, activation='relu'))
+    model.add(Dense(32, activation='relu'))
+    model.add(Dense(1,activation='sigmoid'))
+
+    model.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
+
+    model.fit(X_train, y_train, epochs=1, batch_size=8, verbose=1)
+    model.save("dota_ann.h5")
+
+    y_pred_proba = model.predict(X_test)
+    y_pred = (y_pred_proba > 0.5).astype(int)
+    print('fin apprentissage')
+    print("Accuracy:", accuracy_score(y_test, y_pred))
+    print("Classification Report:\n", classification_report(y_test, y_pred))
+
+if __name__ == '__main__':
+    main()
+
